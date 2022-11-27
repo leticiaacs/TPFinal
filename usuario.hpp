1d2792d83a35cfed24926b198db2dbcf3e0eb37f
na1ara <naiarabarceloss@gmail.com>
2022-11-27 20:41:51 -0300

    hpp da classe usuario rev 1

---
+++ usuario.hpp
@@ -0,0 +1,19 @@
+/** 
+ * @file usuario.hpp
+ * @author Naiara Silveira (naiarabarceloss@gmail.com)
+ * @brief Classe principal com a lógica do usuário
+ * @version 1.0
+ * @date 2022-11-27
+ * @details Responsável por setar e verificar usuários 
+ * e seus parâmetros
+ * @copyright GNU General Public License v2.0
+*/
+
+#ifndef USUARIO_H
+#define USUARIO_H
+
+#include <iostream>
+
+
+
+#endif USUARIO_H
