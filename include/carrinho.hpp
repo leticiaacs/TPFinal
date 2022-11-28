a8e80bfea7afcbe6d3fe5fbe05de9e24aa548d66
na1ara <naiarabarceloss@gmail.com>
2022-11-28 18:36:44 -0300

    hpp da classe carrinho rev 1

---
+++ include/carrinho.hpp
@@ -0,0 +1,26 @@
+/** 
+ * @file carrinho.hpp
+ * @author Leon & Naiara Silveira (naiarabarceloss@gmail.com)
+ * @brief Classe principal com a lógica do carrinho
+ * @version 1.0
+ * @date 2022-11-28
+ * @details Responsável por setar e retornar os parametros 
+ * da compra
+ * @copyright GNU General Public License v2.0
+*/
+
+#ifndef CARRINHO_H
+#define CARRINHO_H
+
+#include <iostream>
+#include <string>
+
+class Carrinho {
+    /**
+     * @brief classe que armazena as compras do usuário
+     * @details parametros e métodos
+     */
+    
+};
+
+#endif
