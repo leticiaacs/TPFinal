a49c4f098c0ac6673e4c5bc689e44c61a39f11f0
leticiaacs <108194166+leticiaacs@users.noreply.github.com>
2022-11-28 20:39:39 -0300

    classe sistema rev. 02

--- include/sistema.hpp
+++ include/sistema.hpp
@@ -8,10 +8,11 @@
 /**
 *@file sistema.hpp
 *@author Letícia Alexandre
-*@brief Classe central do programa, chamada de Sistema e Classe do tratamento de exceções
+*@brief Classe central do programa, chamada de Sistema e Classe na qual está o tratamento de exceção, chamada de DadosInvalidos
 *@version 1.0
 *@date 26/11/2022
-*@details Classe Sistema responsável por realizar o procedimento de cadastro e login da loja de açaí, juntamente com a exibição de produtos disponíveis e Classe DadosInvalidos que faz o tratamento de exceções
+*@details Classe Sistema responsável por realizar os procedimentos iniciais da loja, como a criação de cadastro, o login do cliente
+* e a exibição de informações sobre os produtos (descrição de produtos disponíveis)
 *@copyright GNU General Public License v2.0
 */
 
@@ -19,7 +20,7 @@ class DadosInvalidos : public std::exception {
 
 	/**
 	 * @brief Classe para tratamento de exceções, presente na função de verificar logins
-	 * @details Classe que faz o throw para quando o login inserido pelo usuário não for encontrado no banco de dados.
+	 * @details Classe que faz o throw para quando o login inserido pelo usuário não for encontrado no banco de dados
 	 */
 	
 	private:
@@ -29,7 +30,8 @@ class DadosInvalidos : public std::exception {
 
 	/**
 	 * @brief Construtor da Classe DadosInvalidos
-	 * @details Constrói um novo objeto da Classe DadosInvalidos definindo uma mensagem de erro para o objeto 
+	 * @details Constrói um novo objeto da Classe DadosInvalidos definindo uma mensagem de erro para o objeto para 
+	 * quando a função for chamada.
 	 */
 
 	DadosInvalidos(){
@@ -38,7 +40,8 @@ class DadosInvalidos : public std::exception {
 
 	/**
 	 * @brief Função que retorna a mensagem de erro 
-	 * @details Função que sobrescreve a função what() da Classe mãe std::exception e retorna a mensagem de erro definida no construtor
+	 * @details Função que sobrescreve a função what() da Classe mãe std::exception e retorna a mensagem de erro definida no construtor 
+	 * (Usuario ou senha invalidos. Tente novamente)
 	 * @return const char* mensagem de erro
 	 */
 	
@@ -67,26 +70,29 @@ class Sistema {
 
 	/**
 	 * @brief Função que cria o cadastro
-	 * @details Função que cria o cadastro de um novo usuário, utilizando dados inseridos de cpf e senha e retorna um inteiro para indicar se o cadastro foi bem sucedido ou não
-	 * @param cpf_novo - cpf inserido pelo usuário para cadastro
-	 * @param senha_nova - senha inserida pelo usuário para cadastro
-	 * @return int - 0 para um cadastro bem sucedido e 1 para erro no cadastro
+	 * @details Função que cria o cadastro de um novo usuário, utilizando dados inseridos de cpf e senha e retorna um inteiro para 
+	 * indicar se o cadastro foi bem sucedido ou não
+	 * @param cpf_novo - string cpf com 11 caracteres inseridos pelo usuário para cadastro
+	 * @param senha_nova - string senha inserida pelo usuário para cadastro
+	 * @return inteiro - retorna 0 para um cadastro bem sucedido e 1 para erro no cadastro. Esses dados serão usados na main para instruir o usuario 
+	 * sobre os proximos passos, que dependerão do retorno desta função.
 	 */
-	int cria_cadastro(long int cpf_novo, std::string senha_nova);
+	int cria_cadastro(std::string cpf_novo, std::string senha_nova);
 
 	/**
 	 * @brief Função que verifica o login 
-	 * @details Função que compara o cpf e senha inseridos com os usuários cadastrados no banco de dados para identificar o usuário e emite uma mensagem de erro caso o login não seja localizado
-	 * @param c - cpf inserido pelo usuário para fazer login
-	 * @param s - senha inserida pelo usuário para fazer login
-	 * @return Usuario - usuário identificado pelo login
+	 * @details Função que compara o cpf e senha inseridos com o cpf e senha de usuarios cadastrados no banco de dados para identificar 
+	 * o usuário e emite uma mensagem de erro caso o cadastro não seja localizado. O throw da classe DadosInvalidos se encontra nesta função.
+	 * @param c - string cpf de 11 numeros inseridos pelo usuário para fazer login
+	 * @param s - string senha inserida pelo usuário para fazer login
+	 * @return Usuario - retorna o usuário identificado pelo login, para ser usado em outras funções da main posteriormente
 	 */
-	Usuario verifica_login(long int c, std::string s);
+	Usuario verifica_login(std::string c, std::string s);
 
 	/**
 	 * @brief Função que exibe informações dos produtos
-	 * @details Função que mostra ao usuário todos os produtos disponíveis
-	 * @return std::string - mensagem em formato de string que descreve os produtos disponíveis
+	 * @details Função que mostra ao usuário todos os produtos disponíveis na loja
+	 * @return std::string - mensagem em formato de string que descreve os produtos disponíveis e suas variações
 	 */
 	std::string informacoes_produtos();
 };
