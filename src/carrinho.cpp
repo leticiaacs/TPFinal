e9498aaa852fb20d5b54c771076755e78cd3aaa8
lmfelixs <109525988+lmfelixs@users.noreply.github.com>
2022-11-29 01:10:56 -0300

    Add files via upload

---
+++ src/carrinho.cpp
@@ -0,0 +1,97 @@
+/** 
+ * @file carrinho.cpp
+ * @author Leon Marcos Felix Goncalves & Naiara Silveira (naiarabarceloss@gmail.com)
+ * @brief Classe principal com a lógica do carrinho
+ * @version 1.0
+ * @date 2022-11-28
+ * @details Responsável por setar e retornar os parametros 
+ * da compra
+ * @copyright GNU General Public License v2.0
+*/
+
+#define DESCONTO 0,9
+#include <iostream>
+#include <list>
+#include <string>
+#include <map>
+
+#include "carrinho.hpp"
+#include "usuario.hpp"
+#include "produto.hpp"
+
+
+    void Carrinho::adicionaProduto(Produto a){
+
+        int c=0;
+
+        std::cout<<"Você tem certeza que deseja adicionar este produto ao carrinho? Digite 1 para sim e 2 para nao."<<std::endl;
+        std::cin>>c;
+
+        if(c==1){
+
+        _pedidosProduto.push_back(a);//Adiciona produtos ao final da lista
+        std::cout<<"Produto adicionado a lista com sucesso!"<<std::endl;
+            
+            } else if(c==2){
+
+                std::cout<<"Produto nao adicionado a lista."<<std::endl;
+                }else{
+                    throw Incorreto();
+                     
+                }
+    }
+
+    void Carrinho::removeProduto(Produto a){
+
+        int c=0;
+
+        std::cout<<"Você tem certeza que deseja remover este produto ao carrinho? Digite 1 para sim e 2 para nao."<<std::endl;
+        std::cin>>c;
+
+        if(c==1){
+
+        _pedidosProduto.remove(a);//Remove o produto a
+        std::cout<<"Produto removido da lista com sucesso!"<<std::endl;
+             }else if(c==2){
+
+            std::cout<<"Produto nao removido da lista."<<std::endl;
+
+                }else{
+
+                throw Incorreto();
+
+                }
+        
+
+    }
+
+    float Carrinho::calculaTotal(Usuario u){
+
+        for(std::list<Produto>::iterator it = _pedidosProduto.begin(); it!= _pedidosProduto.end(); it++){
+
+            this->_valorFinal= this->_valorFinal+(*it)->getPreco();//Soma o valor de cada produto da lista
+
+        }
+        if(u.verificaDesconto()!=0){//retorna preco conforme desconto do usuario
+            this->_valorFinal=this->_valorFinal*DESCONTO;
+            return this->valorFinal;
+        } else{
+              return this->valorFinal;
+
+        }
+
+    }
+
+    std::string Carrinho::descricaoFinal(){
+
+        for(std::list<Produto>::iterator it = _pedidosProduto.begin(); it!= _pedidosProduto.end(); it++){
+
+           std::cout<< (*it)->descricao()<<std::endl;;//Exibe a descrição de cada produto da lista
+
+        }
+
+    }
+
+    
+
+    
