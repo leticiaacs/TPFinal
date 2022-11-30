d3b6c7b5ac4e0e4bc7b969d763c10eff76eb9a2c
SkanyCst <yaggovteixeira@gmail.com>
2022-11-30 14:36:03 -0300

    Update Açaí e Vitamina

--- include/acai.hpp
+++ include/acai.hpp
@@ -14,15 +14,18 @@
 *@brief Classe Açaí e Classe do tratamento de exceções
 *@version 1.0
 *@date 27/11/2022
-*@details Classe Acai que usará o vector de Complemento para fazer a montagem do açaí e Classe complementoInvalido que fará o tratamento de exceções
+*@details Classe Acai que usará o vector de Complemento para fazer a 
+*montagem do açaí e Classe complementoInvalido que fará o tratamento 
+*de exceções
 *@copyright GNU General Public License v2.0
 */
 
-class Acai : public Produto {
-
+class Acai : public Produto 
+{
     /**
     * @brief Classe de montagem do açaí.
-    * @details Classe Acai que recebe dois objetos e uma estutura de dados que que será usada para a montagem do açaí.
+    * @details Classe Acai que recebe dois objetos e uma estutura 
+    * de dados que que será usada para a montagem do açaí.
     */
 
     private:
@@ -37,7 +40,7 @@ class Acai : public Produto {
         * @brief Quantidade de açaí's.
         */
 
-        //int _quantidade;
+        int _quantidade;
 
         /**
         * @brief Vector de tipo Complemento que será usado
@@ -49,77 +52,51 @@ class Acai : public Produto {
     public:
 
         /**
-	* @brief Construtor da Classe Acai.
-	* @details Constrói dois novos objetos da Classe Acai.
+	    * @brief Construtor da Classe Acai.
+	    * @details Constrói dois novos objetos da Classe Acai.
         * @param tamanho - tamanho do açaí
         * @param quantidade - quantidade de açaí's
         */
 
-        //Acai(int tamanho, int quantidade);
-        
-        /**
-	* @brief Função que retorna o tamanho do açaí.
-	* @return int - tamanho do açaí.
-	*/
-
-        int getTamanho();
+        Acai(int tamanho, int quantidade);
 
         /**
-	* @brief Função que retorna a quantidade de açaí's.
-	* @return int - quantidade de açaí's.
-	*/
-	
-        int getQuantidade();
-
-        /**
-	* @brief Função que calcula o preço total do açaí.
-        * @details Função que faz a soma total de quantidade, tamanho e complementos do açaí, retornando o valor final.
-	* @return float - preço do açaí.
-	*/
+	    * @brief Função que calcula o preço total do açaí.
+        * @details Função que faz a soma total de quantidade, 
+        * tamanho e complementos do açaí, retornando o valor final.
+	    * @return float - preço do açaí.
+	    */
 
         float calculaPreco() override;
 
         /**
-	* @brief Função que faz a descrição do açaí.
-	* @details Função que faz a leitura de todos os atributos e retorna uma descrição detalhada dos componentes do açaí.
-	* @return std::string - descrição do açaí.
-	*/
+	    * @brief Função que faz a descrição do açaí.
+	    * @details Função que faz a leitura de todos os atributos e 
+        * retorna uma descrição detalhada dos componentes do açaí.
+	    * @return std::string - descrição do açaí.
+	    */
 
         std::string descricao() override;
 
         /**
-	* @brief Função que remove complementos do açaí.
-	* @details Função que acessa o vector de Complemento e remove o último complemento adicionado.
-        * @param nome - nome do complemento a ser removido.
-	*/
-
-        void removeComplemento(std::string nome);
-
-        /**
-	* @brief Função que remove complemento incorreto do açaí.
-	* @details Função que acessa o vector de Complemento e remove o último complemento adicionado.
-	*/
-
-        void removeComplementoIncorreto();
-
-        /**
-	* @brief Função que adiciona complementos ao açaí.
-	* @details Função que acessa o vector de Complemento e adicona um novo complemento ao açaí.
-        * @param nome - nome do complemento a ser adicionado.
-	*/
-
-        void adicionaComplemento(std::string nome);
-
+	    * @brief Função que faz a montagem do açaí.
+	    * @details Função que fará contato iterativo com o cliente para
+        * a montagem do açaí. Receberá o tamanho do açaí desejado, uma 
+        * quantia de 1 a 5 complementos a serem inseridos, remoção de 
+        * complementos a serem removidos do açaí e finalizando quando o
+        * cliente estiver satisfeito com seu pedido.
+	    */
 
         void montar() override;
 };
 
 class complementoInvalido : public std::exception
 {
-
     /**
-    * @brief Classe para tratamento de exceções, presente na função removeComplemento e adicionaComplemento.
-    * @details Classe que faz o throw para quando o nome do complemento inserido não coincidir com as opções.
+    * @brief Classe para tratamento de exceções, presente na função 
+    * montar() no momento de inserir complementos ao açaí.
+    * @details Classe que faz o throw para quando o código do complemento 
+    * inserido não coincidir com as opções.
     */
 
     private:
@@ -133,24 +110,110 @@ class complementoInvalido : public std::exception
     public:
 
         /**
-	* @brief Construtor da Classe complementoInvalido.
-	* @details Constroi um novo objeto para a Classe complementoInvalido e define uma mensagem de erro.
-	*/
+	    * @brief Construtor da Classe complementoInvalido.
+	    * @details Constroi um novo objeto para a Classe 
+        * complementoInvalido e define uma mensagem de erro.
+	    */
 
         complementoInvalido()
         {
-            _msg_erro_complemento = "Complemento inválido. Pressione S se deseja remover o último complemento adicionado ou N se deseja ignorar e seguir em frente.";     
+            _msg_erro_complemento = "Complemento inválido. Por favor, digite novamente o número correspondente ao complemento desejado.";     
         }
 
         /**
-	* @brief Função que retorna uma mensagem de erro.
-	* @details Função que sobrescreve a função what() da Classe mãe std::exception e retorna a mensagem de erro definida no construtor.
-	* @return const char* mensagem de erro.
-	*/
+	    * @brief Função que retorna uma mensagem de erro.
+	    * @details Função que sobrescreve a função what() da Classe mãe 
+        * std::exception e retorna a mensagem de erro definida no construtor.
+	    * @return const char* mensagem de erro.
+	    */
 
         const char* what() const noexcept override
         {
             return _msg_erro_complemento.c_str();
+        }
+};
+
+class limiteDeComplementos : public std::exception
+{
+    /**
+    * @brief Classe para tratamento de exceções.
+    * @details Classe que faz o throw para quando o limite de 
+    * complementos for atingido na função montar().
+    */
+
+    private:
+
+        /**
+        * @brief Mensagem de erro para limite de complementos.
+        */
+
+        std::string _msg_limite_complementos;
+
+    public:
+
+        /**
+	    * @brief Construtor da Classe limiteDeComplementos.
+	    * @details Constroi um novo objeto para a Classe 
+        * limiteDeComplementos e define uma mensagem de erro.
+	    */
+
+        limiteDeComplementos()
+        {
+            _msg_limite_complementos = "Limite de complementos atingido!";
+        }
+
+        /**
+	    * @brief Função que retorna uma mensagem de erro.
+	    * @details Função que sobrescreve a função what() da Classe mãe
+        * std::exception e retorna a mensagem de erro definida no construtor.
+	    * @return const char* mensagem de erro.
+	    */
+
+        const char* what() const noexcept override
+        {
+            return _msg_limite_complementos.c_str();
+        }
+};
+
+class acaiSemComplementos : public std::exception
+{
+    /**
+    * @brief Classe para tratamento de exceções.
+    * @details Classe que faz o throw para quando o usuário tentar 
+    * remover complementos com o açaí já vazio na função montar().
+    */
+
+    private:
+
+        /**
+        * @brief Mensagem de erro para retirada de complementos.
+        */
+
+        std::string _msg_acai_vazio;
+
+    public:
+
+        /**
+	    * @brief Construtor da Classe AcaiSemComplementos.
+	    * @details Constroi um novo objeto para a Classe AcaiSemComplementos 
+        * e define uma mensagem de erro.
+	    */
+
+        acaiSemComplementos()
+        {
+            _msg_acai_vazio = "Remoção inválida, já não existem complementos no açaí.";
+        }
+
+        /**
+	    * @brief Função que retorna uma mensagem de erro.
+	    * @details Função que sobrescreve a função what() da Classe mãe 
+        * std::exception e retorna a mensagem de erro definida no construtor.
+	    * @return const char* mensagem de erro.
+	    */
+
+        const char* what() const noexcept override
+        {
+            return _msg_acai_vazio.c_str();
         }
 };
 #endif
--- src/acai.cpp
+++ src/acai.cpp
@@ -1,4 +1,5 @@
 #include "acai.hpp"
+#include "complemento.hpp"
 
 const float preco_Banana = 1.50;
 const float preco_Granola = 1.50;
@@ -6,15 +7,20 @@ const float preco_Morango = 2.00;
 const float preco_LeiteNinho = 2.50;
 const float preco_LeiteCondensado = 2.50;
 
+Acai::Acai(int tamanho, int quantidade)
+{
+    _tamanho = tamanho;
+    _quantidade = quantidade;
+}
 
-void Acai::montar() {
-
+void Acai::montar() 
+{
     std::string str; 
     char c;
     int n, m = 0;
 
-    std::cout >> "Digite o número de acordo tamanho que você deseja:" >> std::endl >> "1. 300 ml" >> std::endl >> "2. 500 ml" >> std::endl >> "3. 700 ml" >> std::endl;
-    std::cin << n;
+    std::cout << "Digite o número de acordo tamanho que você deseja:" << std::endl << "1. 300 ml" << std::endl << "2. 500 ml" << std::endl << "3. 700 ml" << std::endl;
+    std::cin >> n;
     if(n == 1){
         _tamanho = 300;
     } else if(n == 2){
@@ -22,114 +28,87 @@ void Acai::montar() {
     } else if(n == 3){
         _tamanho = 700;
     } else {
-        throw(escolhaInvalida);//criar escolha invalida
+        throw tamanhoInvalido();
     }
 
-    std::cout >> "Você tem direito a escolher 5 complementos" >> std::endl;
-    do{
-        std::cout >> "Digite o número de acordo com o complemento que você deseja:" >> std::endl;
-        std::cout >> "1. Banana" >> std::endl >> "2. Granola" >> std::endl >> "3. Morango" >> std::endl >> "4. Leite ninho" >> std::endl >> "5. Leite condensado" >> std::endl;
-        std::cin << n;
+    std::cout << "Você tem direito a escolher 5 complementos" << std::endl;
+    do
+    {
+        std::cout << "Digite o número de acordo com o complemento que você deseja:" << std::endl;
+        std::cout << "1. Banana" << std::endl << "2. Granola" << std::endl << "3. Morango" << std::endl << "4. Leite ninho" << std::endl << "5. Leite condensado" << std::endl;
+        std::cin >> n;
 
         if(n == 1){
-            Complemento c = Complemento("Banana");
-            _acrescimos.push_back(c);
+            Complemento a = Complemento("Banana");
+            _acrescimos.push_back(a);
         } else if(n == 2){
-            Complemento c = Complemento("Granola");
-            _acrescimos.push_back(c);
+            Complemento a = Complemento("Granola");
+            _acrescimos.push_back(a);
         } else if(n == 3){
-            Complemento c = Complemento("Morango");
-            _acrescimos.push_back(c);
+            Complemento a = Complemento("Morango");
+            _acrescimos.push_back(a);
         } else if(n == 4){
-            Complemento c = Complemento("Leite ninho");
-            _acrescimos.push_back(c);
+            Complemento a = Complemento("Leite ninho");
+            _acrescimos.push_back(a);
         } else if(n == 5){
-            Complemento c = Complemento("Leite condensado");
-            _acrescimos.push_back(c);
+            Complemento a = Complemento("Leite condensado");
+            _acrescimos.push_back(a);
         } else {
-            throw(escolhaInvalida);//criar escolha invalida
+            throw complementoInvalido();
         }
 
-        std::cout >> "Deseja adicionar mais complementos ao seu açaí? (s/n)?" >> std::endl;
-        std::cin << c;
-        if(c == 's'){
+        std::cout << "Deseja adicionar mais complementos ao seu açaí? (s/n)?" << std::endl;
+        std::cin >> c;
+        if(c == 's')
+        {
             m++;
         } 
-        if(m > 4){
+        if(m > 4)
+        {
             throw limiteDeComplementos();
             c = 'n';
         }
     }while(c == 's');
 
-    std::cout >> "Deseja remover algum complemento? (s/n)?" >> std::endl;
-    std::cin << c;
-    if(c == 's'){
+    std::cout << "Deseja remover algum complemento? (s/n)?" << std::endl;
+    std::cin >> c;
+    if(c == 's')
+    {
         do{
-            std::cout >> "Digite o nome do complemento que deseja remover." >> std::endl;
-            std::cin << str;
+            std::cout << "Digite o nome do complemento que deseja remover." << std::endl;
+            std::cin >> str;
             
-            if(str != "banana" && str != "granola" && str != "morango" && str != "leite ninho" && str != "leite condensado") {
+            if(str != "banana" && str != "granola" && str != "morango" && str != "leite ninho" && str != "leite condensado") 
+            {
                 throw complementoInvalido();
             }
 
             std::vector<Complemento>::iterator it;
-            for(it = _acrescimos.begin(); it != _acrescimos.end(); it++) {
-                
-                if(it->Complemento::getNome() == str){
+            for(it = _acrescimos.begin(); it != _acrescimos.end(); it++)
+            {
+                if(it->Complemento::getNome() == str)
+                {
                     _acrescimos.erase(it);
                     break;
                 }
-                //break;
             }
-            std::cout >> "Deseja remover outro complemento? (s/n)?"
-            std::cin << c;
-            if(_acrescimos.size() == 0){
+            std::cout << "Deseja remover outro complemento? (s/n)?";
+            std::cin >> c;
+            if(_acrescimos.size() == 0)
+            {
                 throw acaiSemComplementos();
                 c = 'n';
             }
         }while(c == 's');
-    } else if(c != 's' && c != 'n'){
+    } 
+    else if(c != 's' && c != 'n')
+    {
         throw escolhaInvalida();
     }
 }
 
-
-int Acai::getQuantidade() {
-    return _quantidade;
-}
-
-
-/*
-void Acai::adicionaComplemento(std::string nome) {
-    Complemento c = Complemento(nome);
-    if(nome != "banana" && nome != "granola" && nome != "morango" && nome != "leite ninho" && nome != "leite condensado")
-    {
-        throw complementoInvalido();
-    }
-    _acrescimos.push_back(c);
-}
-*/
-
-
-void Acai::removeComplemento(std::string nome) {   
-    std::vector<Complemento>::iterator it;
-    for(it = _acrescimos.begin(); it != _acrescimos.end(); it++)
-    {
-        if(it->Complemento::getNome() == nome)
-        {
-            _acrescimos.erase(it);
-        }
-        break;
-    }
-}
-
-
-void Acai::removeComplementoIncorreto() {
-    _acrescimos.pop_back();
-}
-
-
-float Acai::calculaPreco()  {
+float Acai::calculaPreco()  
+{
     float soma;
 
     std::vector<Complemento>::iterator it;
@@ -150,9 +129,10 @@ float Acai::calculaPreco()  {
 }
 
 
-std::string Acai::descricao() {
+std::string Acai::descricao() 
+{
     std::string descricao;
-    descricao = std::to_string(_quantidade);
+    descricao = descricao + std::to_string(_quantidade);
     descricao = descricao + "\n";
     descricao = descricao + std::to_string(_tamanho);
     descricao = descricao + "\n";
