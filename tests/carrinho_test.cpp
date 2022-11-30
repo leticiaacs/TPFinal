8400bf5fbc26cce22345699f42fa77b62f13ed07
na1ara <naiarabarceloss@gmail.com>
2022-11-30 14:21:27 -0300

    escopo de testes da classe carrinho

---
+++ tests/carrinho_test.cpp
@@ -0,0 +1,29 @@
+/** 
+ * @file carrinho_test.cpp
+ * @author Naiara Silveira (naiarabarceloss@gmail.com)
+ * @brief Classe principal com os testes da classe carrinho
+ * @version 1.0
+ * @date 2022-11-30
+ * @details Responsável por testar as funções da classe carrinho
+ * @copyright GNU General Public License v2.0
+*/
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "carrinho.hpp"
+
+TEST_CASE("Testando adicionaProduto"){
+
+}
+
+TEST_CASE("Testando removeProduto"){
+
+}
+
+TEST_CASE("Testando calculaTotal"){
+
+}
+
+TEST_CASE("Testando descricaoFinal"){
+    
+}
