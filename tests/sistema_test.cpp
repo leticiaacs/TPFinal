d07e23510ab0d49ff20bd2f4848778ca836dab6e
na1ara <naiarabarceloss@gmail.com>
2022-11-30 17:12:49 -0300

    parametro de cpf corrigido nas chamadas de funcao

--- tests/sistema_test.cpp
+++ tests/sistema_test.cpp
@@ -1,18 +1,19 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "sistema.hpp"
+#include "usuario.hpp"
 
 TEST_CASE("Testando cria_cadastro"){
    
     SUBCASE("Caso geral: cadastro realizado corretamente"){
         Sistema s = Sistema();
 
-        CHECK_EQ(s.cria_cadastro(12345678911,"AZUL"), 0);
+        CHECK_EQ(s.cria_cadastro("12345678911","AZUL"), 0);
     }
 
     SUBCASE("Caso negativo: cadastro ja existe"){
         Sistema s = Sistema();
-        std::string cpf = 12345678911;
+        std::string cpf = "12345678911";
         REQUIRE(s.cria_cadastro(cpf,"senha"));
 
         CHECK_EQ(s.cria_cadastro(cpf,"AZUL"), 1);
@@ -21,9 +22,9 @@ TEST_CASE("Testando cria_cadastro"){
     SUBCASE("Caso inválido: valores inválidos de cadastro"){
         Sistema s = Sistema();
         
-        CHECK_EQ(s.cria_cadastro(-12345678911,"AZUL"), 1);
-        CHECK_EQ(s.cria_cadastro(00000000000,"azul"), 1);
-        CHECK_EQ(s.cria_cadastro(12,"azul"), 1);
+        CHECK_EQ(s.cria_cadastro("-12345678911","AZUL"), 1);
+        CHECK_EQ(s.cria_cadastro("00000000000","azul"), 1);
+        CHECK_EQ(s.cria_cadastro("12","azul"), 1);
     }
 }
 
@@ -31,18 +32,18 @@ TEST_CASE("Testando verifica_login"){
    
     SUBCASE("Caso geral: login realizado corretamente"){
         Sistema s = Sistema();
-        Usuario u = Usuario(12345678911,"senha",0);
-        REQUIRE(s.cria_cadastro(12345678911,"senha"));
+        Usuario u = Usuario("12345678911","senha",0);
+        REQUIRE(s.cria_cadastro("12345678911","senha"));
 
-        CHECK_EQ(s.verifica_login(12345678911,"senha"), u);
+        CHECK_EQ(s.verifica_login("12345678911","senha"), u);
     }
 
     SUBCASE("Caso negativo: login incorreto"){
         Sistema s = Sistema();
-        REQUIRE(s.cria_cadastro(12345678911,"senha"));
+        REQUIRE(s.cria_cadastro("12345678911","senha"));
 
-        CHECK_THROWS_AS(s.verifica_login(12345678911,"s"), DadosInvalidos);
-        CHECK_THROWS_AS(s.verifica_login(12345678910,"senha"), DadosInvalidos);
+        CHECK_THROWS_AS(s.verifica_login("12345678911","s"), DadosInvalidos);
+        CHECK_THROWS_AS(s.verifica_login("12345678910","senha"), DadosInvalidos);
     }
 
 }
